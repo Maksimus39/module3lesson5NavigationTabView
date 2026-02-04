bd253b3654168d454be7fbb579fe9234d332d492
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-04 19:20:48 +0300

    Initial Commit

---
+++ module3lesson5NavigationTabView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  module3lesson5NavigationTabView
+//
+//  Created by Максим Минаков on 04.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ module3lesson5NavigationTabView/module3lesson5NavigationTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson5NavigationTabViewApp.swift
+//  module3lesson5NavigationTabView
+//
+//  Created by Максим Минаков on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson5NavigationTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
